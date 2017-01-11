e95af972e89c9dbd75e5c099623f3ab16a6a7822
felix.preda <felix.preda@vola.ro>
2017-01-12 00:10:18 +0200

    Put some files

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-csso');
+var watch = require('gulp-watch');
+
+gulp.task('css', function(){
+  gulp.src('./assets/src/*.less')
+    .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./assets/dist/'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./assets/src/*.less' , ['css']);
+});
+
+gulp.task('default', [ 'css', 'watch' ]);
